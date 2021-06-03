deb5542c27cfeec3e3b6d0afbf3d713990260c91
Rahim Shahid <rahim.shahid@rwth-aachen.de>
2021-06-03 15:22:33 +0200

    Implement Log filtering backend and Edge freq filtering on DFG

--- upload_eventlog/views.py
+++ upload_eventlog/views.py
@@ -15,14 +15,16 @@ from pm4py.objects.log.importer.xes import importer as xes_importer_factory
 from pm4py.objects.log.util import dataframe_utils
 from pm4py.objects.conversion.log import converter as log_converter
 from pm4py.objects.conversion.log.variants import to_data_frame as log_to_data_frame
-
+import heapq
+import pm4py
+from pm4py.algo.filtering.dfg import dfg_filtering
 
 # Create your views here.
 
 def upload_page(request):
     log_attributes = {}
-    event_logs_path = os.path.join(settings.MEDIA_ROOT,"event_logs")
-    n_event_logs_path = os.path.join(settings.MEDIA_ROOT,"none_event_logs")
+    event_logs_path = os.path.join(settings.MEDIA_ROOT, "event_logs")
+    n_event_logs_path = os.path.join(settings.MEDIA_ROOT, "none_event_logs")
 
     if request.method == 'POST':
         if request.is_ajax():  # currently is not being used (get commented in html file)
@@ -31,14 +33,14 @@ def upload_page(request):
             file_dir = os.path.join(event_logs_path, filename)
             eventlogs = [f for f in listdir(event_logs_path) if isfile(join(event_logs_path, f))]
 
-            xes_log = xes_importer_factory.apply(file_dir)
-            no_traces = len(xes_log)
-            no_events = sum([len(trace) for trace in xes_log])
+            log = xes_importer_factory.apply(file_dir)
+            no_traces = len(log)
+            no_events = sum([len(trace) for trace in log])
             log_attributes['no_traces'] = no_traces
             log_attributes['no_events'] = no_events
             print(log_attributes)
-            json_respone = {'log_attributes': log_attributes, 'eventlog_list':eventlogs}
-            return HttpResponse(json.dumps(json_respone),content_type='application/json')
+            json_respone = {'log_attributes': log_attributes, 'eventlog_list': eventlogs}
+            return HttpResponse(json.dumps(json_respone), content_type='application/json')
             # return render(request, 'upload.html', {'log_attributes': log_attributes, 'eventlog_list':eventlogs})
         else:
             if "uploadButton" in request.POST:
@@ -61,9 +63,9 @@ def upload_page(request):
                 # log_attributes['no_traces'] = no_traces
                 # log_attributes['no_events'] = no_events
 
-                return render(request, 'upload.html', {'eventlog_list':eventlogs})
+                return render(request, 'upload.html', {'eventlog_list': eventlogs})
 
-            elif "deleteButton" in request.POST: #for event logs
+            elif "deleteButton" in request.POST:  # for event logs
                 if "log_list" not in request.POST:
                     return HttpResponseRedirect(request.path_info)
 
@@ -77,10 +79,10 @@ def upload_page(request):
                 eventlogs.remove(filename)
                 file_dir = os.path.join(event_logs_path, filename)
                 os.remove(file_dir)
-                return render(request, 'upload.html',{'eventlog_list': eventlogs, 'n_eventlog_list': n_eventlogs})
+                return render(request, 'upload.html', {'eventlog_list': eventlogs, 'n_eventlog_list': n_eventlogs})
 
 
-            elif "n_deleteButton" in request.POST: #for none event logs
+            elif "n_deleteButton" in request.POST:  # for none event logs
                 if "n_log_list" not in request.POST:
                     return HttpResponseRedirect(request.path_info)
 
@@ -103,29 +105,35 @@ def upload_page(request):
 
                 file_dir = os.path.join(event_logs_path, filename)
 
-                xes_log = convert_eventfile_to_log(file_dir)
-                
-                no_traces = len(xes_log)
-                no_events = sum([len(trace) for trace in xes_log])
+                log = convert_eventfile_to_log(file_dir)
+
+                # Apply Filters on log
+                # filters = {
+                #     'concept:name': ['Test Repair']
+                # }
+                # log = filter_log(log, filters, True)
+
+                no_traces = len(log)
+                no_events = sum([len(trace) for trace in log])
                 log_attributes['no_traces'] = no_traces
                 log_attributes['no_events'] = no_events
 
-                # Discover DFG
-                from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
-                dfg = dfg_discovery.apply(xes_log)
+                dfg = log_to_dfg(log, 1)
+
                 g6, temp_file = dfg_to_g6(dfg)
                 dfg_g6_json = json.dumps(g6)
 
                 log_attributes['dfg'] = dfg_g6_json
 
                 # Get all the column names and respective values
-                log_attributes['ColumnNamesValues'] = convert_eventlog_to_json(xes_log)
-                
+                log_attributes['ColumnNamesValues'] = convert_eventlog_to_json(log)
+
                 eventlogs = [f for f in listdir(event_logs_path) if isfile(join(event_logs_path, f))]
 
-                return render(request, 'upload.html',{'eventlog_list': eventlogs, 'log_name':filename, 'log_attributes':log_attributes})
+                return render(request, 'upload.html',
+                              {'eventlog_list': eventlogs, 'log_name': filename, 'log_attributes': log_attributes})
 
-            elif "downloadButton" in request.POST: # for event logs
+            elif "downloadButton" in request.POST:  # for event logs
                 if "log_list" not in request.POST:
                     return HttpResponseRedirect(request.path_info)
 
@@ -140,7 +148,7 @@ def upload_page(request):
                 except Exception as e:
                     return None
 
-            elif "n_downloadButton" in request.POST: #for none event logs
+            elif "n_downloadButton" in request.POST:  # for none event logs
                 if "n_log_list" not in request.POST:
                     return HttpResponseRedirect(request.path_info)
 
@@ -166,9 +174,22 @@ def upload_page(request):
         eventlogs = [f for f in listdir(event_logs_path) if isfile(join(event_logs_path, f))]
         n_eventlogs = [f for f in listdir(n_event_logs_path) if isfile(join(n_event_logs_path, f))]
 
-        return render(request, 'upload.html', {'eventlog_list':eventlogs, 'n_eventlog_list': n_eventlogs})
+        return render(request, 'upload.html', {'eventlog_list': eventlogs, 'n_eventlog_list': n_eventlogs})
 
-        #return render(request, 'upload.html')
+        # return render(request, 'upload.html')
+
+
+def log_to_dfg(log, percentage_most_freq_edges):
+    # Discover DFG
+    from pm4py.algo.discovery.dfg import algorithm as dfg_discovery
+    # dfg = dfg_discovery.apply(log)
+    dfg, sa, ea = pm4py.discover_directly_follows_graph(log)
+    activities_count = pm4py.get_attribute_values(log, "concept:name")
+
+    # Filter Frequent Paths
+    dfg, sa, ea, activities_count = dfg_filtering.filter_dfg_on_paths_percentage(dfg, sa, ea, activities_count, percentage_most_freq_edges)
+    return dfg
+
 
 def dfg_to_g6(dfg):
     unique_nodes = []
@@ -183,21 +204,26 @@ def dfg_to_g6(dfg):
         unique_nodes_dict[node] = "node_" + str(index)
 
     nodes = [{'id': unique_nodes_dict[i], 'name': i, 'conf': [
-                                {
-                                    'label': 'Name',
-                                    'value': i
-                                }
-                            ]} for i in unique_nodes_dict]
+        {
+            'label': 'Name',
+            'value': i
+        }
+    ]} for i in unique_nodes_dict]
     freqList = [int(dfg[i]) for i in dfg]
-    maxVal = max(freqList)
-    minVal = min(freqList)
+    maxVal = max(freqList) if len(freqList) != 0 else 0
+    minVal = min(freqList) if len(freqList) != 0 else 0
 
-    edges = [{'source': unique_nodes_dict[i[0]], 'target': unique_nodes_dict[i[1]], 'label': dfg[i], "style": {"lineWidth": ((int(dfg[i]) - minVal)/(maxVal - minVal) *(20-2)+2), "endArrow": True}} for i in
+    edges = [{'source': unique_nodes_dict[i[0]], 'target': unique_nodes_dict[i[1]], 'label': dfg[i],
+              "style": {"lineWidth": ((int(dfg[i]) - minVal) / (maxVal - minVal) * (20 - 2) + 2), "endArrow": True}} for
+             i in
              dfg]
     data = {
         "nodes": nodes,
         "edges": edges,
     }
+
+    # Apply freq filtering on edges
+
     temp_path = os.path.join(settings.MEDIA_ROOT, "temp")
     temp_file = os.path.join(temp_path, 'data.json')
     with open(temp_file, 'w', encoding='utf-8') as f:
@@ -205,44 +231,61 @@ def dfg_to_g6(dfg):
     return data, temp_file
 
 
-def convert_eventfile_to_log(file_path):
+def filter_log(log, filterItemList, isKeepOnlyThese=True):
+    from pm4py.algo.filtering.log.attributes import attributes_filter
+    filtered_log_events = log;
 
+    for key in filterItemList:
+        list_of_values = filterItemList[key]
+        if (type(list_of_values[0]).__name__ == 'int'):
+            filtered_log_events = attributes_filter.apply_numeric_events(filtered_log_events, min(list_of_values),
+                                                                         max(list_of_values), parameters={
+                    attributes_filter.Parameters.ATTRIBUTE_KEY: key,
+                    attributes_filter.Parameters.POSITIVE: isKeepOnlyThese})
+        else:
+            filtered_log_events = attributes_filter.apply(filtered_log_events, list_of_values,
+                                                          parameters={attributes_filter.Parameters.ATTRIBUTE_KEY: key,
+                                                                      attributes_filter.Parameters.POSITIVE: isKeepOnlyThese})
+
+    return filtered_log_events
+
+
+def convert_eventfile_to_log(file_path):
     file_name, file_extension = os.path.splitext(file_path)
 
     if file_extension == '.csv':
-        
-        log = pd.read_csv(file_path, sep = ',')
+
+        log = pd.read_csv(file_path, sep=',')
         log = dataframe_utils.convert_timestamp_columns_in_df(log)
-        #log = log.sort_values('<timestamp_column>')
+        # log = log.sort_values('<timestamp_column>')
         log = log_converter.apply(log)
 
     else:
 
         log = xes_importer_factory.apply(file_path)
-    
-    #df = log_to_data_frame.apply(log)
+
+    # df = log_to_data_frame.apply(log)
     convert_eventlog_to_json(log)
     return log
 
 
 def convert_eventlog_to_json(log):
-
     df = log_to_data_frame.apply(log)
-    
+
     firstIteration = True
     jsonstr = "{ "
     for col in df:
 
         if not firstIteration:
             jsonstr += ", "
-        else:            
+        else:
             firstIteration = False
 
         jsonstr += "\"" + col + "\"" + ": "
-        
-        uniqueSortedData = pd.Series(df[col].unique()).sort_values(ascending = True)
-        uniqueSortedData = uniqueSortedData.reset_index(drop=True) 
-        jsonstr += uniqueSortedData.to_json(orient = "columns", date_format = 'iso')
+
+        uniqueSortedData = pd.Series(df[col].unique()).sort_values(ascending=True)
+        uniqueSortedData = uniqueSortedData.reset_index(drop=True)
+        jsonstr += uniqueSortedData.to_json(orient="columns", date_format='iso')
 
     jsonstr += " }"
 
